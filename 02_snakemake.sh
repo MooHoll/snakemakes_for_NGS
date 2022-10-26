25dd0d1536150ac7a39558a480683fd9176fe619
Hollie Marshall <hollie_marshall@hotmail.co.uk>
2022-10-26 10:30:17 +0100

    update

--- 02_snakemake.sh
+++ 02_snakemake.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #PBS -N snakemake
-#PBS -l walltime=02:00:00
+#PBS -l walltime=24:00:00
 #PBS -l vmem=50gb
 #PBS -m bea
 #PBS -M hollie_marshall@hotmail.co.uk
-#PBS -l nodes=1:ppn=8
+#PBS -l nodes=1:ppn=20
 
 # Run script in the working directory it was submitted in  (8 nodes and 6hrs)
 cd $PBS_O_WORKDIR 
--- WGBS_snakefile.py
+++ WGBS_snakefile.py
@@ -2,16 +2,113 @@ configfile: "config.yaml"
 
 rule all:
         input:
-            expand('{sample}_fastqc.html', sample=config["samples"])
+            'genome_folder/Bisulfite_Genome',
+            'lambda_genome_folder/Bisulfite_Genome',
+            expand('lambda.{sample}_trim_bismark_bt2.bam', sample=config["samples"]),
+            expand('{sample}.CpG_report.merged_CpG_evidence.cov', sample=config["samples"])
 
-rule fastqc:
+rule trimming:
     input:
         "{sample}.fastq"
     output:
-        html="{sample}_fastqc.html",
-        zip="{sample}_fastqc.zip"
-    shell: 
+        "{sample}_trim.fastq"
+    shell:
         """
-        module load fastqc/0.11.5
-        fastqc -t 8 {input}
+        module load trimmomatic/0.36
+        trimmomatic SE -threads 1 {input} {output} \
+        ILLUMINACLIP:illumina_adapters.fa:2:30:10 LEADING:30 TRAILING:30 MINLEN:20 HEADCROP:5
+        """
+
+rule genome_prep:
+    input:
+        "genome_folder"
+    output:
+        "genome_folder/Bisulfite_Genome"
+    shell:
+        """
+        module load bowtie2/2.3.5.1
+        module load samtools/1.9 
+        /scratch/monoallelic/hjm32/bin/Bismark-0.22.3/bismark_genome_preparation {input}
+        """
+
+rule lambda_genome_prep:
+    input:
+        "lambda_genome_folder"
+    output:
+        "lambda_genome_folder/Bisulfite_Genome"
+    shell:
+        """
+        module load bowtie2/2.3.5.1
+        module load samtools/1.9 
+        /scratch/monoallelic/hjm32/bin/Bismark-0.22.3/bismark_genome_preparation {input}
+        """
+
+rule alignment:
+    input:
+        fastq="{sample}_trim.fastq",
+        genome_dir="genome_folder/Bisulfite_Genome" # This is required to make sure previous rules have run
+    output:
+        "{sample}_trim_bismark_bt2.bam"
+    shell:
+        """
+        module load bowtie2/2.3.5.1
+        module load samtools/1.9 
+        /scratch/monoallelic/hjm32/bin/Bismark-0.22.3/bismark \
+        --multicore 3 --genome genome_folder --single_end {input.fastq}
+        """
+
+rule lambda_alignment:
+    input:
+        fastq="{sample}_trim.fastq",
+        genome_dir="lambda_genome_folder/Bisulfite_Genome" # This is required to make sure previous rules have run
+    output:
+        "lambda.{sample}_trim_bismark_bt2.bam"
+    shell:
+        """
+        module load bowtie2/2.3.5.1
+        module load samtools/1.9 
+        /scratch/monoallelic/hjm32/bin/Bismark-0.22.3/bismark \
+        --multicore 3 --genome lambda_genome_folder --prefix lambda --single_end {input.fastq}
+        """
+
+rule deduplication:
+    input:
+        "{sample}_trim_bismark_bt2.bam"
+    output:
+        "{sample}_trim_bismark_bt2.deduplicated.bam"
+    shell:
+        """
+        module load bowtie2/2.3.5.1
+        module load samtools/1.9 
+       /scratch/monoallelic/hjm32/bin/Bismark-0.22.3/deduplicate_bismark {input}
+        """
+
+rule meth_extraction:
+    input:
+        "{sample}_trim_bismark_bt2.deduplicated.bam"
+    output:
+        "{sample}_trim_bismark_bt2.deduplicated.bismark.cov.gz"
+    shell:
+        """
+        module load bowtie2/2.3.5.1
+        module load samtools/1.9 
+        /scratch/monoallelic/hjm32/bin/Bismark-0.22.3/bismark_methylation_extractor \
+        --single-end --no_overlap --comprehensive --bedgraph --report --cytosine_report \
+        --genome_folder genome_folder {input}
+        """
+
+rule merge_cpgs:
+    input:
+        "{sample}_trim_bismark_bt2.deduplicated.bismark.cov.gz"
+    output:
+        "{sample}.CpG_report.merged_CpG_evidence.cov"
+    params:
+        "{sample}"
+    shell:
+        """
+        module load bowtie2/2.3.5.1
+        module load samtools/1.9 
+        /scratch/monoallelic/hjm32/bin/Bismark-0.22.3/coverage2cytosine \
+        -o {params} --merge_CpGs \
+        --genome_folder genome_folder {input}
         """
